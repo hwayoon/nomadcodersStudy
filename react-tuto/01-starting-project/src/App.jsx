9955868dcc7b79ddc07da288ebe9045dab7a5699
{hwayoon] <{rup_rupes@naver.com}>
2024-05-25 21:19:46 +0900

    디렉토리 이동용 커밋

--- react-tuto/01-starting-project/src/App.jsx
+++ react-tuto/01-starting-project/src/App.jsx
@@ -1,33 +1,16 @@
-import reactImg from "./assets/react-core-concepts.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
