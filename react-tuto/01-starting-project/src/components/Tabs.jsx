007e1929480fedae7fe0b6e6997d6e978fa63ce6
hwayoon <149074924+hwayoon@users.noreply.github.com>
2024-06-03 07:45:18 +0900

    Delete react-tuto directory
    Move to new repository: React course

--- react-tuto/01-starting-project/src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Tabs({ children, buttons, buttonsConatiner = "menu" }) {
-  const ButtonsConatiner = buttonsConatiner;
-  return (
-    <>
-      <ButtonsConatiner>{buttons}</ButtonsConatiner>
-      {children}
-    </>
-  );
-}
